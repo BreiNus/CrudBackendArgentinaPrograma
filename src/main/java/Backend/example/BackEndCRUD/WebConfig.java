ea869b7826d13115818cfa2234e8341f6f88d64f
BreiNus <matu_cs_casla@hotmail.com>
2023-05-08 21:10:55 -0300

    Configuracion CORS

---
+++ src/main/java/Backend/example/BackEndCRUD/WebConfig.java
@@ -0,0 +1,16 @@
+package Backend.example.BackEndCRUD;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**");
+
+    }
+}
